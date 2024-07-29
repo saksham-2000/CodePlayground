3f530903cd39b2e3ecb4028652885ffe05974e79
Saksham Garg <sakshamgarg@MacBook-Pro-4.local>
2024-07-29 15:58:43 +0530

    added hover to heading

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,7 +8,7 @@ import nameLogo from './../assets/images/codepen_name_logo.jpg'
 
 function Header() {
   return (
-    <div className='bg-white h-[10vh] w-full flex border-2  border-black m-[2px]'>
+    <div className='hover:bg-gray-300 h-[10vh] w-full flex border-2  border-black m-[2px]'>
    <img src={nameLogo} alt="" className='h-[8vh] pl-4  pt-2 w-[20vw]'/>
     <div className='grow'></div>
    </div>
